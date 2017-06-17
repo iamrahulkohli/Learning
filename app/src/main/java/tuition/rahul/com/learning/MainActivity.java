14c4b0544548bcb896bd499c08a1f9014f4bd1f1
Rahul Kohli <rahul_kohli@yahoo.co.in>
2017-06-17 19:10:45 +0530

    This is Just for Learning Purpose. Please don't copy me. If you have any suggestion. Please help

--- app/src/main/java/tuition/rahul/com/learning/MainActivity.java
+++ app/src/main/java/tuition/rahul/com/learning/MainActivity.java
@@ -1,13 +1,59 @@
 package tuition.rahul.com.learning;
 
+import android.app.Application;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.facebook.login.LoginManager;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
+import tuition.rahul.com.learning.BottomBar.bottombar;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    FirebaseAuth mAuth;
+    public static String TAG="MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+mAuth= FirebaseAuth.getInstance();
+        Log.d(TAG,"Signed out");
+        Button signout= (Button) findViewById(R.id.signout);
+Button go_to_bottom= (Button) findViewById(R.id.bottom);
+        go_to_bottom.setOnClickListener(this);
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mAuth.signOut();
+        Log.d(TAG,"Signed out");
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+
+        mAuth.signOut();
+        Log.d(TAG,"Signed out");
+    }
+
+    @Override
+    public void onClick(View v) {
+        int i=v.getId();
+        if(R.id.signout==i){
+            LoginManager.getInstance().logOut();
+        }
+        else if(R.id.bottom==i){
+            Intent intent=new Intent(this,bottombar.class);
+            startActivity(intent);
+        }
+
     }
 }
